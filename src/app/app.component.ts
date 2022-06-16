510b0b96d971e8aee91944827da998581a0a37ff
enzopcastillo <epcastillov@gmail.com>
2022-06-16 19:53:25 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AyDS2-TP05-Grupo11-12-frontend';
+}
